e03634420d6083664c20e2ec31e49005e01c1251
sanchitchalakh92-ship-it <sanchitchalakh92@gmail.com>
2026-03-26 23:04:23 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,22 @@
+#Arithmetic operators
+a=2
+b=5
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)  #remainder
+
+
+#relational operators
+
+a=20
+b=50
+
+print(a==b)  #False
+print(a!=b)  #True
+print(a>=b)  #False
+print(a>b)  #False
+print(a<=b)  #True
+print(a<b)  #False
