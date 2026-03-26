8962a3997bafbdffb21862c8b4cde30ded083602
sanchitchalakh92-ship-it <sanchitchalakh92@gmail.com>
2026-03-26 22:58:05 +0530

    Add files via upload

---
+++ greter.py
@@ -0,0 +1,12 @@
+a=input("enter first number:")
+b=input("enter second number:")
+c=input("enter third number:")
+
+if(a>b and a>c):
+    print("a is greater ")
+
+elif(b>c):
+    print("b is greater")
+
+else:
+    print("c is greater")
