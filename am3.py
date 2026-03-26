281443d1e2d7890e6a1ab4c6c0ff6d886ac29108
sanchitchalakh92-ship-it <sanchitchalakh92@gmail.com>
2026-03-26 22:50:49 +0530

    Add files via upload

---
+++ am3.py
@@ -0,0 +1,21 @@
+
+a=10
+print(type(a))
+
+b="Sanchit"
+print(type(b))
+
+c=10.55
+print(type(c))
+
+d=["sanchit", 10,10.5]
+print(type(d))
+
+e={"sanchit"}
+print(type(e))
+
+f=("sanchit:",)
+print(type(f))
+
+g={"name": "sanchit", "age": 20}
+print(type(g))
